e9da30cd9f3326efca8cfb2ec45cb3e26645ddf5
Alena Akinshina <akinshina@MacBook-Pro-Alena.local>
2024-10-06 12:27:39 +0300

    Fix errors after test

--- filter_fastq.py
+++ filter_fastq.py
@@ -2,9 +2,9 @@ from scripts.seq_quality_check import gc_content, calculate_average_quality
 
 
 def filter_fastq(seqs: dict[str, tuple[str, str]],
-                 gc_bounds: tuple[float, float],
-                 length_bounds: tuple[int, int],
-                 quality_threshold: int) -> dict[str, tuple]:
+                 gc_bounds: tuple[float, float] = (0, 100),
+                 length_bounds: tuple[int, int] = (0, 2**32),
+                 quality_threshold: int = 0) -> dict[str, tuple]:
 
     if isinstance(gc_bounds, float):
         gc_bounds = (0, gc_bounds)
@@ -12,16 +12,16 @@ def filter_fastq(seqs: dict[str, tuple[str, str]],
     if isinstance(length_bounds, int):
         length_bounds = (0, length_bounds)
 
-        filtered_seqs = {}
+    filtered_seqs = {}
 
-        for name, (sequence, enc_quality) in seqs.items():
-            length = len(sequence) 
-            gc = gc_content(sequence)
-            avg_quality = calculate_average_quality(enc_quality)
+    for seq_id, (sequence, quality) in seqs.items():
+        length = len(sequence) 
+        gc = gc_content(sequence)
+        avg_quality = calculate_average_quality(quality)
 
-            if (length_bounds[0] <= length <= length_bounds[1] and
-                    gc_bounds[0] <= gc <= gc_bounds[1] and
-                    avg_quality >= quality_threshold):
-                filtered_seqs[name] = (sequence, enc_quality)
+        if (length_bounds[0] <= length <= length_bounds[1] and
+                gc_bounds[0] <= gc <= gc_bounds[1] and
+                avg_quality >= quality_threshold):
+            filtered_seqs[seq_id] = (sequence, quality)
 
-        return filtered_seqs
+    return filtered_seqs
