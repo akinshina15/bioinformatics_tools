80785975ffbb4ec82b0e98096028cf6dcade5d0d
Alena Akinshina <akinshina@MacBook-Pro-Alena.local>
2024-10-12 21:29:40 +0300

    Add parse BLAST output function

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -1,3 +1,6 @@
+import os
+
+
 def convert_multiline_fasta_to_oneline(input_fasta, output_fasta=None):
     """
     Converts multiple-line FASTA file into single-line format
@@ -5,7 +8,7 @@ def convert_multiline_fasta_to_oneline(input_fasta, output_fasta=None):
     if output_fasta is None:
         output_fasta = input_fasta.rsplit(".", 1)[0] + "_one_line.fasta"
 
-    with open(input_fasta, "r") as file, open(output_fasta) as out_file:
+    with open(input_fasta, "r") as file, open(output_fasta, "w") as out_file:
         sequence_id = None
         sequence = ""
 
@@ -18,3 +21,30 @@ def convert_multiline_fasta_to_oneline(input_fasta, output_fasta=None):
                 sequence = ""
             else:
                 sequence += line
+
+
+def parse_blast_output(input_file, output_file):
+    """
+    
+    """
+
+    with open(input_file, "r") as file, open(output_file, "w") as out_file:
+        values = set()
+
+        lines = file.readlines()
+        for i in range(len(lines)):
+            line = lines[i].strip()
+            if line.startswith("Sequences producing significant alignments:"):
+                continue
+            if line == "":
+                continue
+            if "Description" in line:
+                next_line = lines[i + 1].strip()
+                parts = next_line.split()
+                protein = ' '.join(parts[:-10])
+                values.add(protein)
+
+        sorted_values = sorted(values)
+
+        for value in sorted_values:
+            out_file.write(value + "\n")
